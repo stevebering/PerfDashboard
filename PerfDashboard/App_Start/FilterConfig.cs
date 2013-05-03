69d01f41ce53104acb2d7e42b34361eef05ca388
Steve Bering <stevebering@gmail.com>
2013-05-03 06:08:30 -0700

    Initial commit of base styles and layouts

---
+++ PerfDashboard/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PerfDashboard
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
